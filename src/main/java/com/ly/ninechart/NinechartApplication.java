b1091590c8b0c032535870168218e4b55bd3ce79
tuoer1995 <2413558497@qq.com>
2019-09-09 22:59:06 +0800

    Initial commit

---
+++ src/main/java/com/ly/ninechart/NinechartApplication.java
@@ -0,0 +1,13 @@
+package com.ly.ninechart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinechartApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run ( NinechartApplication.class , args );
+    }
+
+}
